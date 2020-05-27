5d878c4fa8f89201d70fd893b07e66f32bb50873
Ryan Matlock <ryan.matlock@gmail.com>
2020-05-26 22:10:36 -0700

    improved grouping, customization help, and docstrings

--- eight-ball.el
+++ eight-ball.el
@@ -2,6 +2,10 @@
 ;;
 ;; Implements a Magic 8-Ball function.
 
+(defgroup eight-ball-custom
+  nil
+  "Customization group for EIGHT-BALL (Magic 8-Ball) function")
+
 (defcustom eight-ball-reponses
   '("It is certain."
     "It is decidedly so!"
@@ -25,27 +29,26 @@
     "Very doubtful.")
   "Response possibilities for EIGHT-BALL.
 Default values are standard Magic 8-ball responses."
-  :type 'list
-  :options nil
-  :group nil)
+  :type '(repeat string)
+  :group 'eight-ball-custom)
 
 (defcustom eight-ball-kill-ring-include-timestamp t
   "Include timestamp when adding EIGHT-BALL response to KILL-RING?
 Default: t."
   :type 'boolean
-  :options nil
-  :group nil)
+  :options '(choice (const :tag "Include timestamp" t)
+                    (const :tag "Omit timestamp" nil))
+  :group 'eight-ball-custom)
 
 (defcustom eight-ball-timestamp-format
   "[%F %H:%M:%S]"
   "Timestamp format for EIGHT-BALL response in KILL-RING.
-Default: \"[%F %H:%M:%S]\" (\"[ISO-8601 date⎵hour:min:sec]\")
+Default: \"[%F %H:%M:%S]\" i.e. \"[<ISO-8601 date>⎵<hour(24)>:<min>:<sec>]\"
 See documentation for FORMAT-TIME-STRING for valid values."
   :type 'string
-  :options nil
-  :group nil)
+  :group 'eight-ball-custom)
 
-(message "%s" (format-time-string eight-ball-timestamp-format))  ;; works
+;; (message "%s" (format-time-string eight-ball-timestamp-format))  ;; works
 
 ;; hash question, mod 1000, wait for that many milliseconds (so that question
 ;; content affects time, which affects random seed)
@@ -54,12 +57,13 @@ See documentation for FORMAT-TIME-STRING for valid values."
   "Include question with response in message?
 Default: t."
   :type 'boolean
-  :options nil
-  :group nil)
+  :options '(choice (const :tag "Include question when printing response" t)
+                    (const :tag "Print response only" nil))
+  :group 'eight-ball-custom)
 
 (defun eight-ball (&optional add-to-kill-ring question)
-  "
-Passing the \\[[universal-argument]]"
+  "Prompts user to ask a question, responds like a Magic 8-Ball.
+Passing the \\[[universal-argument]]..."
   (interactive "p\nsAsk the Magic 8-Ball a question: ")
   ;; (message "add-to-kill-ring: %s, question: %s" add-to-kill-ring question)
   (random t)
