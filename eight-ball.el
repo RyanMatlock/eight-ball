dd151104655acda239584d3d3647527e80bccd06
Ryan Matlock <ryan.matlock@gmail.com>
2020-05-27 22:45:35 -0700

    cleaned up documentation a little

--- eight-ball.el
+++ eight-ball.el
@@ -1,6 +1,7 @@
 ;; eight-ball.el
 ;;
-;; Implements a Magic 8-Ball function.
+;; Implements a Magic 8-Ball function, EIGHT-BALL: you ask a question, and it
+;; randomly responds.
 
 (defgroup eight-ball-custom
   nil
@@ -32,7 +33,8 @@ Default values are standard Magic 8-ball responses."
   :type '(repeat string)
   :group 'eight-ball-custom)
 
-(defcustom eight-ball-kill-ring-include-timestamp t
+(defcustom eight-ball-kill-ring-include-timestamp
+  t
   "Include timestamp when adding EIGHT-BALL response to KILL-RING?
 Default: t."
   :type 'boolean
@@ -48,37 +50,8 @@ See documentation for FORMAT-TIME-STRING for valid values."
   :type 'string
   :group 'eight-ball-custom)
 
-;; (message "%s" (format-time-string eight-ball-timestamp-format))  ;; works
-
-;; hash question, mod 1000, wait for that many milliseconds (so that question
-;; content affects time, which affects random seed)
-;; (string-to-number (secure-hash 'sha256 "foo") 16)  ;; hmm, overflow
-;; (string-to-number "ff" 16)  ;; -> 255 (and stuff) (works)
-;; (string-to-number "100" 16)  ;; -> 256 (and stuff) (works)
-;; (message "%d" (string-to-number "100" 16))  ;; -> 256 (works)
-
-;; better than sleeping: add the last few digits of the hash to the random
-;; number, then take the modulo of the length of the responses
-;; or
-;; take the last several digits of the hash, round down to a number divisible by
-;; the length of the available choices, take a random number in that range, and
-;; then take the modulo of that
-
-;; (let* ((hash (secure-hash 'sha256 "foo"))
-;;        (size (length hash))
-;;        (small-hash (substring hash (- size 5) nil))
-;;        (decimal-hash (string-to-number small-hash 16))
-;;        (mod-hash (% decimal-hash 20)))
-;;   (message (concat "hash: %s\n"
-;;                    "small-hash: %s\n"
-;;                    "decimal-hash: %d\n"
-;;                    "mod-hash: %d")
-;;            hash
-;;            small-hash
-;;            decimal-hash
-;;            mod-hash))
-
-(defcustom eight-ball-print-question-with-response t
+(defcustom eight-ball-print-question-with-response
+  t
   "Include question with response in message?
 Default: t."
   :type 'boolean
@@ -88,9 +61,16 @@ Default: t."
 
 (defun eight-ball (&optional add-to-kill-ring question)
   "Prompts user to ask a question, responds like a Magic 8-Ball.
-Passing the \\[[universal-argument]]..."
+
+Passing the \\[[universal-argument]] will add question and response to
+KILL-RING, and if EIGHT-BALL-KILL-RING-INCLUDE-TIMESTAMP is not nil, the
+question/response is preceeded by timestamp formatted per
+EIGHT-BALL-TIMESTAMP-FORMAT.
+
+Why would you want your Magic 8-Ball queries on the KILL-RING? In case you're
+weird like me and log them for the purpose of performing analytics on the timing
+and content of said queries."
   (interactive "P\nsAsk the Magic 8-Ball a question: ")
-  ;; (message "add-to-kill-ring: %s, question: %s" add-to-kill-ring question)
   (random t)
   (let* ((hash (secure-hash 'sha256 question))
          (small-hash (substring hash (- (length hash) 6) nil))
@@ -101,7 +81,7 @@ Passing the \\[[universal-argument]]..."
          ;; be a multiple of size)
          ;; outer mod ensures it's a valid index for eight-ball-responses
          ;;
-         ;; this appears to generate all possibilities; will have to histogram
+         ;; this appears to generate all possibilities; may have to histogram
          ;; results later to feel more confident that this is a reasonable
          ;; approach
          ;; I mean, it's unreasonable in the sense that I could just do
@@ -124,5 +104,3 @@ Passing the \\[[universal-argument]]..."
     (if eight-ball-print-question-with-response
         (message "%s %s" question response)
       (message "%s" response))))
-
-
